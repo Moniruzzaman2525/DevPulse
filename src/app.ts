18305639cb7e5ad3412373585b0b19002a31f4c9
unknown <moniurzzaman25@gmail.com>
2026-07-02 10:34:44 +0600

    start assignemtn

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Express Server running!",
+    author: "Md Moniruzzaman",
+    port: 5000,
+  });
+});
+
+
+
+export default app;
